2d4df6981ba0fd2e0721caadf8dc4b83f9f4556f
rijulsahu <rijul_neo@yahoo.co.in>
2025-11-11 23:08:09 +0530

    fix(form): Update Formspree action URL for email submission

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -124,40 +124,35 @@
   });
 
   function php_email_form_submit(this_form, action, data) {
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: data,
-      timeout: 40000
-    }).done( function(msg){
-      if (msg == 'OK') {
-        this_form.find('.loading').slideUp();
+    fetch(action, {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(response => {
+      this_form.find('.loading').slideUp();
+      if (response.ok) {
         this_form.find('.sent-message').slideDown();
         this_form.find("input:not(input[type=submit]), textarea").val('');
       } else {
-        this_form.find('.loading').slideUp();
-        if(!msg) {
-          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
-        }
-        this_form.find('.error-message').slideDown().html(msg);
+        response.json().then(data => {
+          let error_msg = 'Form submission failed!<br>';
+          if (data && data.errors) {
+            error_msg += data.errors.map(e => e.message).join('<br>');
+          } else if (data && data.error) {
+            error_msg += data.error;
+          }
+          this_form.find('.error-message').slideDown().html(error_msg);
+        }).catch(() => {
+          let error_msg = "Form submission failed! (Could not parse server response)";
+          this_form.find('.error-message').slideDown().html(error_msg);
+        });
       }
-    }).fail( function(data){
-      console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
+    }).catch(error => {
       this_form.find('.loading').slideUp();
+      let error_msg = "Form submission failed!<br>" + error;
       this_form.find('.error-message').slideDown().html(error_msg);
     });
   }
